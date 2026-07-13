9b22ca69e1cbffd80b6accd2049fed5487eb33bf
Muhammad Ehtisham <muhammad.ehtisham@beyonderissolution.com>
2026-07-13 12:08:30 +0500

    added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_APP_API_URL || "http://localhost:8000";
+export const API_URL = import.meta.env.VITE_APP_API_URL || "http://go-loundry-backend.onrender.com/" || "http://localhost:8000";
